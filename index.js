00abb98d1496bf113d01dc079ad47193ca6d3e5e
Jose <gldmz0318@icloud.com>
2022-11-26 15:12:09 -0800

    added callback funtion

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
+
+function handleDirectionChange(){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+    move(character).withArrowKeys(100, 250, handleDirectionChange)
+// move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+// let direction = null;
+// let x = 100;
+// let y = 250;
 
 setInterval(function(){
     if(direction === 'west') {
@@ -43,7 +65,7 @@ document.addEventListener('keydown', function(e){
     }
 })
 
-document.addEventListener('keyup', funtion(e){
+document.addEventListener('keyUp', function(e){
     direction = null
 })
 
